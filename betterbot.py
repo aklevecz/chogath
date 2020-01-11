fe4b5d904496647d17f78d66e87b6abe66813d7a
aklevecz <aklevecz@meowwolf.com>
2020-01-10 17:45:24 -0700

    rosey

--- betterbot.py
+++ betterbot.py
@@ -1,4 +1,7 @@
 # from win32gui import GetWindowText, GetForegroundWindow, GetWindowRect
+from multiprocessing import Process, Value, Array, Manager
+import threading
+import ctypes
 from PIL import ImageGrab, Image
 import pytesseract
 import cv2
@@ -6,6 +9,9 @@ import numpy as nm
 import time
 import pyautogui
 import random
+import imagehash
+from random import random
+
 
 def keyMove(k, t):
     for key in k:
@@ -13,6 +19,7 @@ def keyMove(k, t):
     time.sleep(t)
     for key in k:
         pyautogui.keyUp(key)
+
 
 def moveToDoor():
     print('door move')
@@ -22,9 +29,8 @@ def moveToDoor():
     # keyMove('a', 1)
     # keyMove('w', 3.5)
     # --------------------
-    keyMove(['w','a'], 3.261)
+    keyMove(['w', 'a'], 3.261)
     keyMove('w', 7.442)
-
 
 
 def moveToField():
@@ -38,37 +44,47 @@ def moveToField():
     # keyMove('d', 15)
     # keyMove('w', 1.5)
 
-    keyMove(['w','a'], 1.629)
+    keyMove(['w', 'a'], 1.629)
     keyMove('w', 4)
     keyMove(['right'], .33)
+    time.sleep(1)
     pyautogui.hotkey('shift', 'g')
-    time.sleep(5)
-    for i in range(0,6):
+    time.sleep(6)
+    for i in range(0, 6):
         keyMove('w', 10)
         jump()
+
 
 def castNova():
     pyautogui.press('3')
 
+
 def castAE():
     pyautogui.press('e')
+
 
 def castShield():
     pyautogui.press('r')
 
+
 def jump():
     pyautogui.press('space')
 
-def castInt():
+
+def drinkWater():
     pyautogui.press('l')
 
-def castRuby():
+
+def castWater():
     pyautogui.press(';')
+
 
 def randomSpell():
     time.sleep(1)
     spells = [castNova, castAE, castShield, jump]
-    spells[random.randint(0,3)]()
+    spells[random.randint(0, 3)]()
+
+
 def moveOutOfCave():
     castShield()
     time.sleep(3)
@@ -91,39 +107,96 @@ def moveOutOfCave():
     castShield()
     releaseSpirit()
     pyautogui.keyUp('w')
+
+
 def releaseSpirit():
-    pyautogui.moveTo(690,197)   
-    pyautogui.leftClick()
+    releaseSpirit = ImageGrab.grab(bbox=(1300, 380, 1500, 420))
+    rs_string = pytesseract.image_to_string(
+        cv2.cvtColor(nm.array(releaseSpirit), cv2.COLOR_BGR2GRAY),
+        lang='eng')
+    print(rs_string)
+    if rs_string == 'Release Spirit':
+        print(rs_string)
+        pyautogui.moveTo(690, 197)
+        pyautogui.leftClick()
+
 
 def targetBM():
-    time.sleep(3)
-    pyautogui.press('enter')
-    pyautogui.typewrite('/tar Grizzle Halfmane')
-    pyautogui.press('enter')
-    pyautogui.press('\\')
-    pyautogui.moveTo(310,258)
-    time.sleep(1)
-    pyautogui.leftClick()
-    pyautogui.moveTo(310,485)
-    time.sleep(1)
-    pyautogui.leftClick()
+    nugget = ImageGrab.grab(bbox=(2330, 340, 2355, 368))
+    hash = imagehash.average_hash(nugget)
+    template = imagehash.average_hash(Image.open('nugget.jpg'))
+    diff = hash - template
+    print(diff)
+    if diff > 10:
+        time.sleep(3)
+        pyautogui.press('p')
+        pyautogui.press('\\')
+        pyautogui.moveTo(310, 260)
+        time.sleep(1)
+        pyautogui.leftClick()
+        pyautogui.moveTo(310, 485)
+        time.sleep(1)
+        pyautogui.leftClick()
+    else:
+        return True
+    return False
+
 
 def joinBattle():
     time.sleep(2)
-    pyautogui.moveTo(630,205)
-    pyautogui.leftClick()
+    jb = ImageGrab.grab(bbox=(1200, 380, 1400, 420))
+    jb_string = pytesseract.image_to_string(
+        cv2.cvtColor(nm.array(jb), cv2.COLOR_BGR2GRAY),
+        lang='eng')
+    print(jb_string)
+    if jb_string == "Enter Battle" or jb_string == "Teta orsteeC Sy" or jb_string == "elt tae et aeles" or jb_string == "Frnter Battle":
+        pyautogui.moveTo(630, 205)
+        pyautogui.leftClick()
+        return True
+    return False
+
 
 def leaveBattle():
-    time.sleep(2)   
-    pyautogui.moveTo(700,540)
-    pyautogui.leftClick()
+    lb = ImageGrab.grab(bbox=(1350, 1080, 1550, 1120))
+    lb_str = pytesseract.image_to_string(
+        cv2.cvtColor(nm.array(lb), cv2.COLOR_BGR2GRAY),
+        lang='eng')
+    time.sleep(1)
+    if lb_str == "Leave Battleground":
+        pyautogui.moveTo(700, 540)
+        pyautogui.leftClick()
+        return True
+    return False
 
-from multiprocessing import Process, Value, Array, Manager
-import ctypes
-import threading
 
-    
+#  ACCEPT
+# cap =ImageGrab.grab(bbox=(1200, 380, 1400, 420))
+#
+# CANCEL
+# ImageGrab.grab(bbox=(1300, 380, 1500, 420))
+# if cancel & alterac valley, in cave again
 
+# almost the nugger
+# cap = ImageGrab.grab(bbox=(2330,340,2355,368))
+
+# cap = ImageGrab.grab(bbox=(2120,130,2170,180))
+
+# REleASE SPIRIT
+# cap = ImageGrab.grab(bbox=(1300, 380, 1500, 420))
+
+# ENTER BATTLE
+# ImageGrab.grab(bbox=(1200, 380, 1400, 420))
+
+# almost leave BG
+# cap = ImageGrab.grab(bbox=(1350, 1080, 1550, 1120))
+
+# is queued ?
+# import imagehash
+# cap = ImageGrab.grab(bbox=(2320,330,2370,380))
+# hash = imagehash.average_hash(cap)
+# otherhash = imagehash.average_hash(Image.open('nugget.jpg'))
+# if less than 10
+# print(hash - otherhash)
 if __name__ == "__main__":
     def set_interval(func, sec):
         def func_wrapper():
@@ -134,30 +207,31 @@ if __name__ == "__main__":
         return t
 
     zone = Manager().Value(ctypes.c_char_p, 'zone')
+    ZONE = 'Royal Quarter'
 
     def processZone():
-        cap = ImageGrab.grab(bbox=(2300,100,2500,124))
+        cap = ImageGrab.grab(bbox=(2300, 100, 2500, 124))
         # cap.show()
-        tesstr = pytesseract.image_to_string( 
-                cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),  
-                lang ='eng')
-        cap2 = ImageGrab.grab(bbox=(2300,100,2500,124))
+        tesstr = pytesseract.image_to_string(
+            cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),
+            lang='eng')
+        cap = ImageGrab.grab(bbox=(2300, 100, 2500, 124))
         # cap.show()
-        tesstr2 = pytesseract.image_to_string( 
-                cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),  
-                lang ='eng')
-        
-        cap = ImageGrab.grab(bbox=(2300,100,2500,124))
+        tesstr2 = pytesseract.image_to_string(
+            cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),
+            lang='eng')
+
+        cap = ImageGrab.grab(bbox=(2300, 100, 2500, 124))
         # cap.show()
-        tesstr3 = pytesseract.image_to_string( 
-                cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),  
-                lang ='eng')
+        tesstr3 = pytesseract.image_to_string(
+            cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY),
+            lang='eng')
         try:
             print([tesstr, tesstr2, tesstr3])
         except:
             print('bad printing')
-        if 'Royal Quarter' in [tesstr, tesstr2, tesstr3]:
-            zone.value = 'Royal Quarter'
+        if ZONE in [tesstr, tesstr2, tesstr3]:
+            zone.value = ZONE
         else:
             zone.value = tesstr3
 
@@ -168,40 +242,88 @@ if __name__ == "__main__":
         print(zone.value)
     except:
         print('bad printing')
-    wow_window = False
+
     running = True
-    if zone.value == 'Royal Quarter':
+    drink_counter = 0
+    if zone.value == ZONE:
+        print('not in av to start')
         origin = 'HR'
+        queued = False
+        joined = False
+        left = True
     else:
+        print('av to start')
         origin = 'AV'
-    #origin = 'HR'
+        queued = True
+        joined = True
+        left = False
+    origin = 'HR'
+    move_counter = 0
     while running is True:
         processZone()
-        if zone.value == 'Royal Quarter':
-            targetBM()
-            castRuby()
-            joinBattle()
-            time.sleep(30)
-            castInt()
+        print(f'q: {queued}, j: {joined}, l:{left}')
+        if (zone.value == ZONE and queued == False and joined == False) or left == True:
+            move_counter = 0
+            print('queueing')
+            left = False
+            queue = targetBM()
+            queued = queue
             origin = 'HR'
+        elif queued == True and joined == False:
+            print(f'waiting to join, drink_counter:{drink_counter}')
+            if queued == True:
+                pyautogui.press('/')
+            join = joinBattle()
+            joined = join
+            if drink_counter > 5:
+                castWater()
+            time.sleep(5)
+            if drink_counter > 5:
+                drinkWater()
+                drink_counter = 0
+            drink_counter += 1
         else:
             if origin == 'HR':
+                joined = False
+                queued = False
                 time.sleep(15)
-                if zone.value != 'Royal Quarter':
+                if zone.value != ZONE:
+                    queue_counter = 0
                     moveToDoor()
-                    time.sleep(60)
-                    moveToField()
                     time.sleep(90)
+                    moveToField()
                     origin = 'AV'
-            if "Graveyard" in zone.value:
-                time.sleep(30)
+            if "Graveyar" in zone.value or "Winterax Hold" in zone.value:
+                move_counter = 0
+                ghosted = True
+                while ghosted == True:
+                    cancel = ImageGrab.grab(bbox=(1300, 380, 1500, 420))
+                    cancel_string = pytesseract.image_to_string(
+                        cv2.cvtColor(nm.array(cancel), cv2.COLOR_BGR2GRAY),
+                        lang='eng')
+                    print(cancel_string)
+                    if cancel_string == 'Cancel':
+                        time.sleep(5)
+                    else:
+                        ghosted = False
                 pyautogui.hotkey('shift', 'g')
                 time.sleep(5)
-                keyMove('d', 20)
-            leaveBattle()
-            randomSpell()
-            time.sleep(31)
-            if "Graveyard" not in zone.value:
-                releaseSpirit()
-            leaveBattle()
+                if zone.value != ZONE:
+                    if 'blood' in zone.value or "Snowfall" in zone.value:
+                        keyMove('d', 20)
+                    else:
+                        keyMove('w', 20)
+            time.sleep(2)
+            leave = leaveBattle()
+            castWater()
+            if left == False and move_counter < 3:
+                keyMove(['right'], random())
+                keyMove('w', 10)
+
+                leave = leaveBattle()
+                move_counter += 1
+            time.sleep(5)
+            releaseSpirit()
+            drinkWater()
+            left = leave
             time.sleep(5)
--- wowbot/bin/activate.fish
+++ wowbot/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ariel.klevecz/wowbot/wowbot"
+set -gx VIRTUAL_ENV "/Users/arielklevecz/wowbot/wowbot"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(wowbot) "            
+        if test -n "(wowbot) "
             printf "%s%s" "(wowbot) " (set_color normal)
         else
             # ...Otherwise, prepend env
